c451482c873fa88869d9d3111d9ea7af0d368626
Fun <fun_wudy@163.com>
2017-05-10 23:32:15 +0800

    complete first test with @ComponentScan etc.

---
+++ src/main/java/stereosystem/soundsystem/CDPlayerConfig.java
@@ -0,0 +1,10 @@
+package stereosystem.soundsystem;
+import org.springframework.context.annotation.*;
+
+/**
+ * Created by Fun on 2017/5/10.
+ */
+@Configuration
+@ComponentScan
+public class CDPlayerConfig {
+}
---
+++ src/main/java/stereosystem/soundsystem/CompactDisc.java
@@ -0,0 +1,8 @@
+package stereosystem.soundsystem;
+
+/**
+ * Created by Fun on 2017/5/10.
+ */
+public interface CompactDisc {
+    void play();
+}
